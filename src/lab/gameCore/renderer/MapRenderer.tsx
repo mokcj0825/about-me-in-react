5b2c4df4759b4afc821fa6642188f4c67cb0f1f1
mokcj0825 <mokcjmok@gmail.com>
2025-04-10 11:41:21 +0800

    Establish core (map)

--- src/lab/gameCore/renderer/MapRenderer.tsx
+++ src/lab/gameCore/renderer/MapRenderer.tsx
@@ -4,7 +4,8 @@ import { GridRenderer } from './GridRenderer';
 import { createHexCoordinate, HexCoordinate } from '../types/HexCoordinate';
 import { ScrollConfig } from '../system-config/ScrollConfig';
 import { GridLayout } from '../system-config/GridLayout';
-import { MapBorders } from './MapBorders';
+import { MapBorder } from '../component/MapBorder';
+import {calculateNewPosition, Position, ScrollDirection} from "./map-utils";
 
 interface MapData {
   width: number;
@@ -12,27 +13,28 @@ interface MapData {
   terrain: TerrainType[][];
 }
 
-interface MapRendererProps {
+interface Props {
   mapFile: string;
 }
 
-export const MapRenderer: React.FC<MapRendererProps> = ({ mapFile }) => {
+export const MapRenderer: React.FC<Props> = ({ mapFile }) => {
   const [mapData, setMapData] = useState<MapData | null>(null);
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
   const mapRef = useRef<HTMLDivElement | null>(null);
   const scrollInterval = useRef<number | null>(null);
 
-  useEffect(() => {
-    const loadMapData = async () => {
-      try {
-        const map = await import(`../map-data/${mapFile}.json`);
-        setMapData(map);
-      } catch (error) {
-        console.error('Failed to load map data:', error);
-      }
-    };
+  const loadMapData = async () => {
+    try {
+      const map = await import(`../map-data/${mapFile}.json`);
+      setMapData(map);
+    } catch (error) {
+      console.error('Failed to load map data:', error);
+    }
+  };
 
+  useEffect(() => {
     loadMapData();
+
     return () => {
       if (scrollInterval.current !== null) {
         window.clearInterval(scrollInterval.current);
@@ -47,34 +49,21 @@ export const MapRenderer: React.FC<MapRendererProps> = ({ mapFile }) => {
   const { width, height } = mapData;
   const mapWidth = width * GridLayout.WIDTH + GridLayout.ROW_OFFSET + (ScrollConfig.PADDING * 2);
   const mapHeight = height * GridLayout.WIDTH * 0.75 + (ScrollConfig.PADDING * 2);
+  const mapDimension = {x: mapWidth, y: mapHeight}
 
-  const handleScroll = (direction: 'left' | 'right' | 'up' | 'down') => {
+  const handleScroll = (direction: ScrollDirection) => {
     if (scrollInterval.current !== null) {
       window.clearInterval(scrollInterval.current);
     }
 
     scrollInterval.current = window.setInterval(() => {
       setPosition(prev => {
-        const newPos = { ...prev };
-        const viewportWidth = mapRef.current?.clientWidth || 0;
-        const viewportHeight = mapRef.current?.clientHeight || 0;
-        
-        switch (direction) {
-          case 'left':
-            newPos.x = Math.min(ScrollConfig.SCROLL_THRESHOLD, prev.x + ScrollConfig.SCROLL_SPEED);
-            break;
-          case 'right':
-            newPos.x = Math.max(-(mapWidth - viewportWidth), prev.x - ScrollConfig.SCROLL_SPEED);
-            break;
-          case 'up':
-            newPos.y = Math.min(ScrollConfig.SCROLL_THRESHOLD, prev.y + ScrollConfig.SCROLL_SPEED);
-            break;
-          case 'down':
-            newPos.y = Math.max(-(mapHeight - viewportHeight), prev.y - ScrollConfig.SCROLL_SPEED);
-            break;
+
+        const viewportDimension = {
+          x: mapRef.current?.clientWidth || 0,
+          y: mapRef.current?.clientHeight || 0
         }
-        
-        return newPos;
+        return calculateNewPosition(prev, direction, mapDimension, viewportDimension);
       });
     }, 16); // ~60fps
   };
@@ -86,53 +75,17 @@ export const MapRenderer: React.FC<MapRendererProps> = ({ mapFile }) => {
     }
   };
 
-  const renderHex = (coordinate: HexCoordinate) => {
-    return (
-      <GridRenderer
-        key={`${coordinate.x},${coordinate.y}`}
-        coordinate={coordinate}
-        terrain={mapData.terrain[coordinate.y][coordinate.x] as TerrainType}
-      />
-    );
-  };
-
   const grid = generateGrid(width, height);
 
   return (
-    <div
-      ref={mapRef}
-      style={{
-        width: '100%',
-        height: '100%',
-        position: 'relative',
-        userSelect: 'none',
-        overflow: 'hidden'
-      }}
-    >
-      <MapBorders onScroll={handleScroll} onStopScroll={handleStopScroll} />
-      <div style={{
-        padding: `${ScrollConfig.PADDING}px`,
-        width: `${mapWidth}px`,
-        height: `${mapHeight}px`,
-        margin: 0,
-        boxSizing: 'border-box',
-        position: 'absolute',
-        zIndex: 1,
-        transform: `translate(${position.x}px, ${position.y}px)`,
-        transition: 'transform 0.1s linear'
-      }}>
+    <div ref={mapRef} style={wrapperStyle}>
+      <MapBorder onScroll={handleScroll} onStopScroll={handleStopScroll} />
+      <div style={mapSheetStyle(mapDimension, position)}>
         {grid.map((row, index) => (
-          <div key={index} style={{
-            display: 'flex',
-            margin: 0,
-            padding: 0,
-            lineHeight: 0,
-            fontSize: 0,
-            whiteSpace: 'nowrap',
-            marginLeft: (height - 1 - index) % 2 === 0 ? `${GridLayout.ROW_OFFSET}px` : '0',
-            marginTop: index === 0 ? '0' : '-25px',
-          }}>
-            {row.map((coord) => renderHex(coord))}
+          <div key={index} style={gridStyle(height, index)}>
+            {
+              row.map((coordinate) => renderHex(coordinate, mapData.terrain[coordinate.x][coordinate.y] as TerrainType))
+            }
           </div>
         ))}
       </div>
@@ -150,4 +103,49 @@ const generateGrid = (width: number, height: number) => {
     grid.push(row);
   }
   return grid;
-}; 
+};
+
+const renderHex = (coordinate: HexCoordinate, terrain: TerrainType) => {
+  return (
+    <GridRenderer
+      key={`${coordinate.x},${coordinate.y}`}
+      coordinate={coordinate}
+      terrain={terrain}
+    />
+  );
+};
+
+const wrapperStyle = {
+  width: '100%',
+  height: '100%',
+  position: 'relative',
+  userSelect: 'none',
+  overflow: 'hidden'
+} as const;
+
+const mapSheetStyle = (mapDimension: Position, position: Position) => {
+  return {
+    padding: `${ScrollConfig.PADDING}px`,
+    width: `${mapDimension.x}px`,
+    height: `${mapDimension.y}px`,
+    margin: 0,
+    boxSizing: 'border-box',
+    position: 'absolute',
+    zIndex: 1,
+    transform: `translate(${position.x}px, ${position.y}px)`,
+    transition: 'transform 0.1s linear'
+  } as const;
+}
+
+const gridStyle = (height: number, index: number) => {
+  return {
+    display: 'flex',
+    margin: 0,
+    padding: 0,
+    lineHeight: 0,
+    fontSize: 0,
+    whiteSpace: 'nowrap',
+    marginLeft: (height - 1 - index) % 2 === 0 ? `${GridLayout.ROW_OFFSET}px` : '0',
+    marginTop: index === 0 ? '0' : '-25px',
+  }
+}
