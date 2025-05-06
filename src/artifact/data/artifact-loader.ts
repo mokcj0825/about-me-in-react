cdd12477a100047cf67dfe4bdfea549ab6b78a79
mokcj0825 <mokcjmok@gmail.com>
2025-05-06 15:52:41 +0800

    Add two sets

--- src/artifact/data/artifact-loader.ts
+++ src/artifact/data/artifact-loader.ts
@@ -2,12 +2,15 @@ import { ArtifactSet, Artifact } from '../types/ArtifactTypes';
 import gladiatorSet from './sets/gladiator-finale.json';
 import wandererSet from './sets/wanderer-troupe.json';
 import independenceSet from './sets/independence-legacy.json';
-
+import goldenAgeSet from './sets/golden-age.json';
+import fallOfLighthouseSet from './sets/fall-of-lighthouse.json';
 // Add new sets here as they are created
 const artifactSetFiles = [
   gladiatorSet,
   wandererSet,
-  independenceSet
+  independenceSet,
+  goldenAgeSet,
+  fallOfLighthouseSet
 ];
 
 // Type assertion for imported JSON
