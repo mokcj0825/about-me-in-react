b02a6e9bac4daf4cafcb7b1a9396c4c6e8e2a94e
CJ Mok <cjmok@zooloo.asia>
2025-07-19 02:05:29 +0800

    NPM test for draggable

--- src/lab/experiments/npmTest/NpmLab.tsx
+++ src/lab/experiments/npmTest/NpmLab.tsx
@@ -3,15 +3,15 @@ import {Draggable, DraggableFrame} from "react-draggable-frame";
 const NpmLab = () => {
   return (
       <div>
-          <Draggable id={"0001"} anchored={true} >
+          <DraggableFrame id={"0001"} anchored={true} >
               <div onClick={() => {
                   alert('Clicked');
               }}>Hello</div>
-          </Draggable>
+          </DraggableFrame>
 
-          <Draggable id={"0002"} anchored={true} >
+          <DraggableFrame id={"0002"} defaultPosition={{x: 20, y: 40}} anchored={true} >
               <div>Hello 2</div>
-          </Draggable>
+          </DraggableFrame>
       </div>
   )
 }
